d0d00e18b5ced27b7ce6cf27431267f3579b338a
Aman Kumar <124803086+Amandf@users.noreply.github.com>
2025-12-09 23:21:24 +0530

    .java Medium

---
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int peopleAwareOfSecret(int n, int delay, int forget) {
+        final int MOD = 1_000_000_007;
+        long share = 0;
+        int[] dp = new int[n];
+        dp[0] = 1;
+
+        for (int i = 1; i < n; ++i) {
+            if (i - delay >= 0) {
+                share = (share + dp[i - delay]) % MOD;
+            }
+            if ( i - forget >= 0) {
+                share = (share - dp[i - forget] + MOD) % MOD;  
+            }
+            dp[i] = (int) share;
+        }
+
+        long result = 0;
+
+        for (int i = n - forget; i < n; ++i) {
+            result = (result + dp[i]) % MOD;
+        }
+        return (int) result;
+    }
+}
