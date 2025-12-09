e39a7350e88d973bddf19e8569ae368459bfbe00
Aman Kumar <124803086+Amandf@users.noreply.github.com>
2025-12-10 00:20:25 +0530

    .java Easy

---
+++ 3136-valid-word/3136-valid-word.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3) return false;
+
+        boolean hasVowel = false, hasConsonant = false;
+        String vowels = "aeiouAEIOU";
+
+        for (char c : word.toCharArray()) {
+            if (!Character.isLetterOrDigit(c)) return false;
+
+            if (Character.isLetter(c)) {
+                if (vowels.indexOf(c) >= 0) hasVowel = true;
+                else hasConsonant = true;
+            }
+        }
+        return hasVowel && hasConsonant;
+    }
+}
