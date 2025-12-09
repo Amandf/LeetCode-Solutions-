1b0f25d6530eb95ced56f5c80aa9c304f396aa2f
Aman Kumar <124803086+Amandf@users.noreply.github.com>
2025-12-09 23:20:06 +0530

    .java Medium

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+        int n = spells.length;
+        int m = potions.length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            long required = (success + spells[i] - 1) / spells[i]; // ceiling division
+            int index = binarySearch(potions, required);
+            result[i] = m - index;
+        }
+
+        return result;
+    }
+
+    private int binarySearch(int[] potions, long target) {
+        int left = 0, right = potions.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (potions[mid] < target) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+}
