c322f3d20ec1b0eb6beec9bd7513db0663a18cb1
Aman Kumar <124803086+Amandf@users.noreply.github.com>
2026-01-30 14:24:23 +0530

    .java Hard

--- 2977-minimum-cost-to-convert-string-ii/2977-minimum-cost-to-convert-string-ii.java
+++ 2977-minimum-cost-to-convert-string-ii/2977-minimum-cost-to-convert-string-ii.java
@@ -1,113 +1,77 @@
 class Solution {
+    int len = 0;
+    public long minimumCost(String source, String target, String[] original, String[] changed, int[] cost) {
+        Integer index=0;
+        Trie root= new Trie();
+        //adding all string to get index ( like adding to arraylist)
+        for (int i=0; i<original.length;i++) {
+            root.get(original[i]);root.get(changed[i]);
+        }
+        //adjacent matrix for original to target
+        int n = len;
+        int[][] adj = new int[n][n];
+        for(int i=0;i<n;i++) {Arrays.fill(adj[i],Integer.MAX_VALUE);adj[i][i]=0;}
+        for(int i=0;i<cost.length;i++) {
+            int oriIndex=root.get(original[i]), chaIndex=root.get(changed[i]);
+            adj[oriIndex][chaIndex]=Math.min(adj[oriIndex][chaIndex],cost[i]);
+        }
+        //as a->b, b->c is three thena->c will be there so doing Dloyd warshall algo
+        floydWarshall(adj);
+        return helper(source.toCharArray(),target.toCharArray(),adj,root);
+    }
 
-   static class Node {
-       String s;
-       int cost;
-       Node(String s, int cost) {
-           this.s = s;
-           this.cost = cost;
-       }
-   }
+    public long helper(char[] s, char[] t, int[][] adj,Trie root){
+        int m=s.length;
+        long[] dp= new long[m + 1];
+        for(int i=0; i<=m;i++)dp[i]=Long.MAX_VALUE;
+        dp[0]=0;
 
+        for(int i=0;i<m; i++) {
+            if(dp[i]==Long.MAX_VALUE)continue;
+            if(s[i]==t[i])dp[i+1]=Math.min(dp[i+1],dp[i]);
 
-   public long minimumCost(String source, String target,
-                           String[] original, String[] changed, int[] cost) {
-
-
-       int n = source.length();
-       long INF = (long) 1e18;
-
-
-        // Group substring by length
-       Map<Integer, List<Integer>> byLength = new HashMap<>();
-       for (int i = 0; i < original.length; i++) {
-           int len = original[i].length();
-           byLength.computeIfAbsent(len, k -> new ArrayList<>()).add(i);
-       }
-
-
-        // For each length, build graph & run Floyd
-       Map<Integer, Map<String, Map<String, Long>>> minSubCost = new HashMap<>();
-
-
-       for (int len : byLength.keySet()) {
-           List<Integer> ids = byLength.get(len);
-
-
-           Set<String> nodes = new HashSet<>();
-           for (int id : ids) {
-               nodes.add(original[id]);
-               nodes.add(changed[id]);
-           }
-
-
-           Map<String, Map<String, Long>> dist = new HashMap<>();
-           for (String a : nodes) {
-               dist.put(a, new HashMap<>());
-               for (String b : nodes) {
-                   dist.get(a).put(b, a.equals(b) ? 0L : INF);
-               }
-           }
-
-
-           for (int id : ids) {
-               String u = original[id];
-               String v = changed[id];
-               dist.get(u).put(v,
-                   Math.min(dist.get(u).get(v), (long) cost[id]));
-           }
-
-
-           for (String k : nodes)
-               for (String i : nodes)
-                   for (String j : nodes) {
-                       long nd = dist.get(i).get(k) + dist.get(k).get(j);
-                       if (nd < dist.get(i).get(j)) {
-                           dist.get(i).put(j, nd);
-                       }
-                   }
-
-
-           minSubCost.put(len, dist);
-       }
-
-
-        // Step 4: DP
-       long[] dp = new long[n + 1];
-       Arrays.fill(dp, INF);
-       dp[n] = 0;
-
-
-       for (int i = n - 1; i >= 0; i--) {
-
-
-           if(source.charAt(i) == target.charAt(i)){
-               dp[i] = dp[i+1];
-           }
-
-
-           for (int len : minSubCost.keySet()) {
-               if (i + len > n) continue;
-
-
-               String subS = source.substring(i, i + len);
-               String subT = target.substring(i, i + len);
-
-
-               Map<String, Map<String, Long>> dist = minSubCost.get(len);
-               if (!dist.containsKey(subS)) continue;
-
-
-               long c = dist.get(subS).getOrDefault(subT, INF);
-               if (c < INF) {
-                   dp[i] = Math.min(dp[i], c + dp[i + len]);
-               }
-           }
-       }
-
-
-       return dp[0] >= INF ? -1 : dp[0];
-   }
+            //as we are searching from root everytime
+            Trie souTrie=root,tarTrie=root;
+            inner: for(int j=i;j<m;j++) {
+                //if we get index from source and target we can find adj[i][j] of that
+                souTrie=souTrie.child[(int)(s[j]-'a')];
+                tarTrie=tarTrie.child[(int)(t[j]-'a')];
+                //if we do not have any source or target then there is no point of getting further
+                if(souTrie==null || tarTrie == null) {break inner;}
+                if(souTrie.val!=-1 && tarTrie.val!=-1 & adj[souTrie.val][tarTrie.val] != Integer.MAX_VALUE){
+                    dp[j+1]=Math.min(dp[j+1],adj[souTrie.val][tarTrie.val]+dp[i]);
+                }
+            }
+        }
+        return dp[m]==Long.MAX_VALUE?-1:dp[m];
+    }
+    public void floydWarshall(int [][] adj){
+        int n =adj.length;
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                if(adj[i][k]!=Integer.MAX_VALUE)
+                  for(int j=0;j<n;j++){
+                    if(adj[k][j]!=Integer.MAX_VALUE && adj[i][j]>adj[i][k]+adj[k][j]){
+                        adj[i][j]=adj[i][k]+adj[k][j];
+                    } 
+                  }
+            }
+        }
+    }
+    class Trie{
+        int val;
+        Trie[] child;
+        Trie(){child=new Trie[26];val=-1;}
+        int get(String str){//serves as both add or get str
+            char[] chstr =str.toCharArray();
+            Trie temp=this;
+            for(int i=0;i<chstr.length;i++){
+                int index=(int)(chstr[i]-'a');
+                if(temp.child[index]==null)temp.child[index]=new Trie();
+                temp=temp.child[index];
+            }
+            if(temp.val==-1){temp.val=len++;}
+            return temp.val;
+        }
+    }
 }
-
-
