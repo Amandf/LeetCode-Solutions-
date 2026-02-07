f7be4e4b533ee88f81b02e0df935ddd7daefa3c3
Aman Kumar <124803086+Amandf@users.noreply.github.com>
2026-02-07 11:44:12 +0530

    .java Medium

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,18 @@
+//Keeping count of 'b'
+//For each 'a', choose min cost: delete all previous 'b's OR delete this 'a'
+class Solution {
+    public int minimumDeletions(String s) {
+        int n = s.length();
+        int bcount = 0;
+        int ans = 0;
+
+        for(char ch : s.toCharArray()) {
+            if(ch=='a') {
+                //Either delete all 'b's before this 'a', or delete this 'a'
+                ans = Math.min(bcount, 1+ans);
+            }
+            else bcount++;
+        }
+        return ans;
+    }
+}
