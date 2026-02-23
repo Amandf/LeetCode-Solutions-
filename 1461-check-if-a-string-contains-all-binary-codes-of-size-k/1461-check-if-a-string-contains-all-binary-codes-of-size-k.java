d30a2995a52b89ee4d4a4ca9938948d8f3cfbd07
Aman Kumar <124803086+Amandf@users.noreply.github.com>
2026-02-23 12:04:18 +0530

    .java Medium

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean hasAllCodes(String s, int k) {
+        int n = s.length();
+
+        if(n < k) {
+            return false;
+        }
+
+        int required = 1 << k;
+        Set<String> seenPatterns = new HashSet<>();
+
+        for(int end = k; end <= n; end++) {
+            String current = s.substring(end - k, end);
+
+            if(seenPatterns.add(current)) {
+                required--;
+            }
+            if(required == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
