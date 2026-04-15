21f1506bbd7974b368e2acc9a7806cd89fb3dbf4
Aman Kumar <124803086+Amandf@users.noreply.github.com>
2026-04-15 20:06:09 +0530

    .java Easy

---
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int closestTarget(String[] words, String target, int start) {
+        int n = words.length;
+        int ans = n+1;
+        // O(n), O(1)
+        for(int i=0; i<n; i++) {
+            if(words[i].equals(target))
+            {
+                int dist1 = Math.abs(start-i);
+                int dist2 = n - dist1;
+                ans = Math.min(ans, Math.min(dist1, dist2));
+            }
+        }
+
+        if(ans > n)
+            return -1;
+        return ans;
+
+    }
+}
