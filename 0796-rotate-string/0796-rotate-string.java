3d59347dd333f9d13c8794d558b3a5db384ccda8
Aman Kumar <124803086+Amandf@users.noreply.github.com>
2026-05-03 23:56:11 +0530

    .java Easy

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,15 @@
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+
+        if(goal.length()!=s.length())
+            return false;
+        
+        String temp = s + s;
+
+        if(temp.indexOf(goal)!=-1)
+            return true;
+        return false;
+
+    }
+}
